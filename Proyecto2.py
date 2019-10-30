94579dafef39c778d440ff2b3801878c61af28ea
Kevin <kevin_said@outlook.com>
2019-10-30 17:47:05 -0600

    Impresiones actualizadas
    Se actualizaron las impresiones

--- Proyecto2.py
+++ Proyecto2.py
@@ -24,31 +24,39 @@ class Reader ():
 
 lector = Reader()
 temporal = input("Inserta el archivo: ")
-contador = 1
+contador1 = 1
+contador2 = 2
 contadorVariables = 0
 contadorLineas = 0
-archivo = lector.reader(temporal)
+archivo = lector.readerPalabras(temporal)
 archivo2 = lector.reader(temporal)
+print("Variables que no cunplen con el estandar")
 for i in archivo:
     if (i == "String" or i == "byte" or i == "int" or i == "float" 
     or i == "double" or i == "char" or i == "boolean" or i == "void" or i == "new"
     or i == "class"):
-        cadena = archivo[contador]
+        cadena = archivo[contador1]
         cadena = lector.splitter(cadena)
+        print(cadena,len(cadena))
         if len(cadena) < 3:
             print(cadena)
             contadorVariables = contadorVariables + 1
-    contador = contador + 1
+    contador1 = contador1 + 1
 
+print("")
+print("Lineas extensas que no cumplen con el estandar")
 for i in archivo2:
     caracteres = len(i.strip())
     if(caracteres > 100):
         print(i.strip())
         contadorLineas = contadorLineas + 1
+    cibtadir2 = contador2+2
 
-
+print("")
 print ("La cantidad de nombres de variables cortas no descriptivas que se encontraron son: ",contadorVariables)
 print ("La cantidad de lineas que exceden el estandar de longitud 100 es: ", contadorLineas)
+print ("Total de variables encontradas: ", contador1-1)
+print ("Total de lineas encontradas: ", contador2-2)
 #(
 #.
 #,
